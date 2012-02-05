6d2748218a00aa30facf48ff71038b403bfca212
Yury Benesh <ybxsoft@tut.by>
2012-02-06 00:03:51 +0300

    Directory scanner base.

---
+++ fpc/common/palitsa_scanner.pas
@@ -0,0 +1,63 @@
+unit palitsa_scanner;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TBaseDirScanner = class;
+
+  TDirScannerEnterEvent = procedure(o : TBaseDirScanner; path, name : string) of object;
+  TDirScannerFoundEvent = procedure(o : TBaseDirScanner; var f : TSearchRec) of object;
+
+  TBaseDirScanner = class(TObject)
+  private
+    FOnEnterDirectory : TDirScannerEnterEvent;
+    FOnFoundEntry : TDirScannerFoundEvent;
+  public
+    property OnEnterDirectory : TDirScannerEnterEvent read FOnEnterDirectory write FOnEnterDirectory;
+    property OnFoundEntry : TDirScannerFoundEvent read FOnFoundEntry write FOnFoundEntry;
+
+
+    procedure SearchFrom(path : string);virtual;abstract;
+
+  end;
+
+
+  /// factory function
+  function CreateBaseDirScanner : TBaseDirScanner;
+
+implementation
+
+// 1) TSearchRec is always single. Scan the directory first and add directory names to a list.
+// Then iterate over the list and search for descendant directory's contents.
+// 2) Use many TSearchRec and employ recursive calls.
+
+type
+
+  { TImplDirScanner }
+
+  TImplDirScanner = class(TBaseDirScanner)
+    public
+      procedure SearchFrom(path : string);override;
+
+  end;
+
+
+function CreateBaseDirScanner : TBaseDirScanner;
+begin
+  result := TImplDirScanner.Create;
+end;
+
+{ TImplDirScanner }
+
+procedure TImplDirScanner.SearchFrom(path: string);
+begin
+  //inherited SearchFrom(path);
+end;
+
+end.
+
--- fpc/palitsa_scan/palitsa_scan.lpr
+++ fpc/palitsa_scan/palitsa_scan.lpr
@@ -6,7 +6,7 @@ uses
   {$IFDEF UNIX}{$IFDEF UseCThreads}
   cthreads,
   {$ENDIF}{$ENDIF}
-  Classes, SysUtils, CustApp, palitsa_base
+  Classes, SysUtils, CustApp, palitsa_base, palitsa_scanner
   { you can add units after this };
 
 type
