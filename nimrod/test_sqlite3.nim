92b0183698791cdf6f48eaf182e8c2c56c788ba6
Yury Benesh <ybxsoft@tut.by>
2012-09-26 23:57:37 +0300

    SQL Script execution test.

--- nimrod/test_sqlite3.nim
+++ nimrod/test_sqlite3.nim
@@ -1,10 +1,20 @@
 import db_sqlite
+import os
+
+import sqlutils
 
 var
     mydb : TDbConn
     
-mydb = db_sqlite.open("test.sqdb", "", "", "")
+let dbFile = "test.sqdb"
+let dbScript = "db_schema1_sqlite.sql"
 
+removeFile dbFile
+mydb = db_sqlite.open(dbFile, "", "", "")
+let script = parseSqlFile(dbScript)
+for i in script:
+  echo "executing '" & i & "'..."
+  db_sqlite.exec mydb, TSqlQuery(i)
 
 db_sqlite.close(mydb)
     
