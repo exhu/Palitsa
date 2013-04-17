cded7c23196c822c66be71860332ab26f54426c0
Yury Benesh <ybxsoft@tut.by>
2013-04-17 23:40:13 +0300

    testing macro WIP

--- nimrod/tests/testdsmacro.nim
+++ nimrod/tests/testdsmacro.nim
@@ -1,7 +1,10 @@
 # test dataset macro
 import macros
+import times
 
 import palitsa_sqlutils
+
+discard """
 
 template dataSet(body: stmt): stmt {.immediate.}=
     template col*(f: expr, t: typedesc, c: string): stmt {.immediate.}=
@@ -18,4 +21,36 @@ dataSet:
     col scanTime, TTime, "scan_time"
     col rootId, TEntityId, "root_id"
     
+"""
+
+macro defDataset(stmts: stmt): stmt =
+    nil
     
+#template fieldCol(c: string, f: string, t: typedesc) =
+#    nil
+
+#dumpTree:
+#    fieldCol "id", "id", TEntityId ## SQL=sss
+    
+
+{.pragma: id_sql.}
+dumpTree:
+    type
+        TMyDs = object
+            ## used for inserting new entries and querrying.
+            ## FIELD ORDER IS IMPORTANT!
+            id {.id_sql.}: TEntityId # SQL=id
+            name: string # SQL=name
+            originalPath: string # SQL=original_path
+            scanTime: TTime # SQL=scan_time
+            rootId: TEntityId # SQL=root_id
+
+dumpTree:
+    type 
+        TDirEntryDesc* = tuple
+            ## used for inserting new entries and querrying.
+            ## FIELD ORDER IS IMPORTANT!
+            id: TEntityId
+            parentId: TEntityId
+            path: string
+            name: string
