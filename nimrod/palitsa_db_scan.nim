28badfe05284f591dee66b10934b24149408f747
Yury Benesh <ybxsoft@tut.by>
2013-06-03 00:42:43 +0300

    Implemented addFsTree, needs testing

--- nimrod/palitsa_db_scan.nim
+++ nimrod/palitsa_db_scan.nim
@@ -1,29 +1,50 @@
+## this module scans dirs and populates db
+
 import os
-import palitsa_db, palitsa_scan
+import palitsa_db, palitsa_scan, palitsa_sqlutils
 
-# this module scans dirs and populates db
 
-proc addTree*(parent: TEntityId, path: string) =
-## Scans filesystem and adds files/directories to specified 
-## 'parent' TDirEntryDesc which is usually a generated root_id received
-## from createMedia().
+proc addEntry(o: ptr TOpenDb, curDir: TEntityId, fn: string, 
+    isDir: bool): TEntityId =
+
+    var e: TDirEntryDesc
+    e.parentId = curDir
+    let (dir, name, ext) = fn.splitFile
+    e.path = dir
+    e.name = name & ext
+    e.fileSize = fn.getFileSize()
+    e.mTime = fn.getLastModificationTime()
+    e.isDir = isDir
+    
+    o[].createEntry(e)
+
+
+proc addFsTree*(o: var TOpenDb, parent: TEntityId, path: string) =
+    ## Scans filesystem and adds files/directories to specified 
+    ## 'parent' TDirEntryDesc which is usually a generated root_id received
+    ## from createMedia().
     echo "addTree"
     var 
         sif: TScanIface
         curDir = parent
         dirStack: seq[TEntityId] = @[]
         
-    sif.onNewFile = proc(fn:string) =
-        echo "TODO add file"
+    let pO = addr(o) # some hack -) i know the o will be alive in the following
+    # closures and we cannot pass it as var, so let's pass it as ptr...
+        
+    sif.onNewFile = proc(fn:string) =        
+        discard addEntry(pO, curDir, fn, isDir = false)
         
     sif.onEnterDir = proc(fn:string) =
-        dirStack.push(curDir)
-        # TODO create entity for dir, set curDir
+        dirStack.add(curDir)
+        curDir = addEntry(pO, curDir, fn, isDir = true)        
         
-    sif.onLeaveDir = proc(fn:string) =
-        echo "TODO pop curDir"
+    sif.onLeaveDir = proc(fn:string) =        
         curDir = dirStack.pop()
 
-    scanPath(path, sif)
+    inTransaction(o):
+        scanPath(path, sif)
     
+
+# TODO write unit test
 
