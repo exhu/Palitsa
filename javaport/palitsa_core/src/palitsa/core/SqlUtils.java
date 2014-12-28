e45227a1eae6a6e7e6e1952ec24a6a05af53e660
Yury Benesh <ybxsoft@tut.by>
2014-12-28 16:41:28 +0300

    palitsa_core new java project

---
+++ javaport/palitsa_core/src/palitsa/core/SqlUtils.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palitsa.core;
+
+/**
+ *
+ * @author yur
+ */
+public final class SqlUtils {
+    public static final long NULL_ID = 0;
+    
+}
--- javaport/thirdparty/README.txt
+++ javaport/thirdparty/README.txt
@@ -1,2 +1,9 @@
-Sqlite3 jdbc
-JOpt
+Sqlite3 jdbc:
+https://bitbucket.org/xerial/sqlite-jdbc/
+sqlite-jdbc-3.8.7.jar
+
+JOpt Simple: 
+https://pholser.github.io/jopt-simple/
+jopt-simple-4.8.jar
+jopt-simple-4.8-javadoc.jar
+
